1c07af1c9cc1bd4ce4890a145d35361d5407b18b
= <=>
2024-02-13 22:18:52 -0300

    feat: Finalized L3

--- main.js
+++ main.js
@@ -2,7 +2,11 @@ const app = Vue.createApp({
     data(){
         return {
             product: 'Meias',
-            descriptionProduct: 'Estas meias são importadas diretamente da gringa'
+            descriptionProduct: 'Estas meias são importadas diretamente da gringa',
+            image: './assets/images/socks_blue.jpg',
+            alternative: 'Meias azuis com logomarca Vue',
+            url: 'https://vuejs.org',
+            target: '_blank'
         }
     }
 })
